158f663990ddbaf3fd420807ac02293057dc28cb
Samantha Kerr <samantha-kerr@hotmail.ca>
2018-10-27 21:55:07 -0400

    #6 Define given database attributes

--- config/db.php
+++ config/db.php
@@ -11,12 +11,7 @@
  * DB_USER: user for your database. the user needs to have rights for SELECT, UPDATE, DELETE and INSERT.
  * DB_PASS: the password of the above user
  */
-/*define("DB_HOST", "vdc353.encs.concordia.ca");
+define("DB_HOST", "vdc353.encs.concordia.ca");
 define("DB_NAME", "vdc353_2");
 define("DB_USER", "vdc353_2");
-define("DB_PASS", "jrssv353");*/
-
-define("DB_HOST", "localhost");
-define("DB_NAME", "soen353");
-define("DB_USER", "root");
-define("DB_PASS", "Squ1rrel!!!");
+define("DB_PASS", "jrssv353");
