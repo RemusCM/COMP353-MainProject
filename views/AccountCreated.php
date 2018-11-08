89fe92e2332aec34aabd9c70f5f435761a3b4957
Remus Mocanu <Remus_C.M@hotmail.com>
2018-11-08 15:34:58 -0500

    #10 Added the first part (if a client makes a checkings or savings account)

--- views/AccountCreated.php
+++ views/AccountCreated.php
@@ -1,6 +1,6 @@
 <?php
 //Assume new account balance is always 0
-
+session_start();
 $connect = mysqli_connect('vdc353.encs.concordia.ca', 'vdc353_2','jrssv353','vdc353_2');
 
 if (!$connect) {
@@ -11,6 +11,9 @@ $level = $_POST['level'];
 $serviceType = $_POST['service-type'];
 $accountType = $_POST['account-type'];
 $interestRate = 0.0;
+$balance = 0.00;
+//need to complete registration of a client/log-in to retrieve this
+$clientID = $_SESSION['client_id'];
 
 
 // Course of action if user picks Checkings or savings
@@ -20,17 +23,37 @@ if($accountType == 'checking' || $accountType = 'Savings') {
         $chargePlan = $_POST['charge-plan'];
 
         //Insert new record into account
+        $query = "INSERT INTO Account(client_id, balance, account_type, service_type, option_name, interest_rate)
+                          VALUES('$clientID', '$balance', '$accountType', '$serviceType', '$chargePlan', '$interestRate');";
+
+        mysqli_query($connect, $query);
+
 
         //Insert new record into Checking
+        //Retrieve first the last inserted ID.
+        $accountNumberQuery = "SELECT LAST_INSERT_ID();";
+        $accountNumber = mysqli_query($connect, $accountNumberQuery);
+        $query = "INSERT INTO Checking(account_number, opt) VALUES('$accountNumber', '$chargePlan')";
 
+        mysqli_query($connect, $query);
     }
+
     elseif ($accountType == 'savings'){
         $interestRate = 2.0;
         $chargePlan = $_POST['charge-plan'];
 
         //Insert new record into account
+ $query = "INSERT INTO Account(client_id, balance, account_type, service_type, option_name, interest_rate)
+                          VALUES('$clientID', '$balance', '$accountType', '$serviceType', '$chargePlan', '$interestRate');";
 
+        mysqli_query($connect, $query);
         //Insert new record into Savings
+        $accountNumberQuery = "SELECT LAST_INSERT_ID();";
+        $accountNumber = mysqli_query($connect, $accountNumberQuery);
+        $query = "INSERT INTO Checking(account_number, opt) VALUES('$accountNumber', '$chargePlan')";
+
+        mysqli_query($connect, $query);
+
     }
 
 }
@@ -51,6 +74,10 @@ elseif($accountType = 'credit'){
 
     //For every limit amount, there's a different interest rate
 
+    //Insert a new record into account
+
+    //Insert a new record into credit
+
 }
 
 //Course of action for loan
@@ -59,11 +86,13 @@ elseif($accountType =='loan'){
     $loanType = $_POST['loan-type'];
     $loanLimit = $_POST['loan-limit'];
 
-    //For different types of limit, we have different  interest rates
+    //For different amounts of limit, we have different interest rates
 
+    //Insert a new record into account
+
+    //Insert a new record into Loan
 
 }
 
 mysqli_close($connect);
 
-?>
