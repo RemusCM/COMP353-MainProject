0c3775f730d16bbcd1ca590a82777970c129540f
Remus Mocanu <Remus_C.M@hotmail.com>
2018-11-09 00:40:31 -0500

    #10 Finished basic functionality. Need time guards for credit card and line of credit, Also need to contact stan to help us connect to actual server. Finally, need registration/login of client complete so we can use their specific client_id, I used a hard-coded id.

--- views/AccountCreated.php
+++ views/AccountCreated.php
@@ -1,7 +1,8 @@
 <?php
 //Assume new account balance is always 0
 session_start();
-$connect = mysqli_connect('vdc353.encs.concordia.ca', 'vdc353_2','jrssv353','vdc353_2');
+
+$connect = mysqli_connect("localhost", "root","","testAccount");
 
 if (!$connect) {
     die("Connection failed: " . mysqli_connect_error());
@@ -12,30 +13,35 @@ $serviceType = $_POST['service-type'];
 $accountType = $_POST['account-type'];
 $interestRate = 0.0;
 $balance = 0.00;
-//need to complete registration of a client/log-in to retrieve this
-$clientID = $_SESSION['client_id'];
 
+
+//need to complete registration of a client/log-in to retrieve this
+//TODO store client_id in session once logged in. Also store joining date to see if he is able to make credit card/line of credit
+//$clientID = $_SESSION['client_id'];
+//$clientJoinDate = $_SESSION['joining_date'];
+
+$client_id = 1; //Testing with fake/ hard-coded client_id until registration/login provides client info to session
 
 // Course of action if user picks Checkings or savings
-if($accountType == 'checking' || $accountType = 'Savings') {
+if($accountType == 'checking' || $accountType == 'savings') {
     if($accountType == 'checking'){
         $interestRate = 0.0;
         $chargePlan = $_POST['charge-plan'];
 
-        //Insert new record into account
-        $query = "INSERT INTO Account(client_id, balance, account_type, service_type, option_name, interest_rate)
-                          VALUES('$clientID', '$balance', '$accountType', '$serviceType', '$chargePlan', '$interestRate');";
 
-        mysqli_query($connect, $query);
+        //Insert new record into Account and Checking
+        $query = "INSERT INTO Account(client_id, balance, account_type, service_type, level, interest_rate)
+                          VALUES('$client_id', '$balance', '$accountType', '$serviceType', '$level', '$interestRate');";
 
 
         //Insert new record into Checking
         //Retrieve first the last inserted ID.
-        $accountNumberQuery = "SELECT LAST_INSERT_ID();";
-        $accountNumber = mysqli_query($connect, $accountNumberQuery);
-        $query = "INSERT INTO Checking(account_number, opt) VALUES('$accountNumber', '$chargePlan')";
+        if(mysqli_query($connect,$query)) {
+            $accountNumber = mysqli_insert_id($connect);
+            $query1 = "INSERT INTO Checking(account_number, opt) VALUES('$accountNumber', '$chargePlan');";
+            mysqli_query($connect, $query1);
+        }
 
-        mysqli_query($connect, $query);
     }
 
     elseif ($accountType == 'savings'){
@@ -43,16 +49,17 @@ if($accountType == 'checking' || $accountType = 'Savings') {
         $chargePlan = $_POST['charge-plan'];
 
         //Insert new record into account
- $query = "INSERT INTO Account(client_id, balance, account_type, service_type, option_name, interest_rate)
-                          VALUES('$clientID', '$balance', '$accountType', '$serviceType', '$chargePlan', '$interestRate');";
+ $query = "INSERT INTO Account(client_id, balance, account_type, service_type, level, interest_rate)
+                          VALUES('$client_id', '$balance', '$accountType', '$serviceType', '$level', '$interestRate');";
 
-        mysqli_query($connect, $query);
+
         //Insert new record into Savings
-        $accountNumberQuery = "SELECT LAST_INSERT_ID();";
-        $accountNumber = mysqli_query($connect, $accountNumberQuery);
-        $query = "INSERT INTO Checking(account_number, opt) VALUES('$accountNumber', '$chargePlan')";
-
-        mysqli_query($connect, $query);
+        //The if statement actually executes the query, no need to execute before
+        if(mysqli_query($connect,$query)) {
+            $accountNumber = mysqli_insert_id($connect);
+            $query1 = "INSERT INTO Savings(account_number, opt) VALUES('$accountNumber', '$chargePlan');";
+            mysqli_query($connect, $query1);
+        }
 
     }
 
@@ -60,24 +67,64 @@ if($accountType == 'checking' || $accountType = 'Savings') {
 //Course of action for foreign currency
 
 elseif($accountType == 'foreign-currency'){
+
+
     $currency = $_POST['currency'];
 
     //Insert a new record into account
-
+    $query = "INSERT INTO Account(client_id, balance, account_type, service_type, level, interest_rate)
+                          VALUES('$client_id', '$balance', '$accountType', '$serviceType', '$level', '$interestRate');";
     //Insert a new record into Foreign currency
+    if(mysqli_query($connect,$query)) {
+        $accountNumber = mysqli_insert_id($connect);
+
+        $query1 = "INSERT INTO ForeignCurrency(account_number, currency_type ) VALUES('$accountNumber', '$currency');";
+        mysqli_query($connect, $query1);
+    }
 
 }
 
 //Course of action for credit
-elseif($accountType = 'credit'){
+
+elseif($accountType == 'credit'){
     $creditLimit = $_POST['credit-limit'];
 
-    //For every limit amount, there's a different interest rate
-
+    //For every limit amount and service type, there's a different interest rate
+    if($serviceType == 'banking'){
+        $interestRate += 2.0;
+    }
+    elseif ($serviceType =='investment'){
+        $interestRate += 1.0;
+    }
+    elseif ($serviceType == 'insurance'){
+        $interestRate += 0.5;
+    }
+    if($creditLimit == '500.00'){
+        $interestRate += 2.0;
+        $minimumPayment = 25.00; //I understand minimal_payment as a percentage of what you owe.
+    }
+    elseif($creditLimit == '1000.00'){
+        $interestRate += 1.5;
+        $minimumPayment = 20.00;
+    }
+    elseif($creditLimit == '5000.00'){
+        $interestRate += 1.0;
+        $minimumPayment = 15.00;
+    }
+    elseif($creditLimit == '10000.00'){
+        $interestRate += 0.5;
+        $minimumPayment = 10.00;
+    }
     //Insert a new record into account
-
+    $query = "INSERT INTO Account(client_id, balance, account_type, service_type, level, interest_rate)
+                          VALUES('$client_id', '$balance', '$accountType', '$serviceType', '$level', '$interestRate');";
     //Insert a new record into credit
+    if(mysqli_query($connect,$query)) {
+        $accountNumber = mysqli_insert_id($connect);
 
+        $query1 = "INSERT INTO Credit(account_number, credit_limit, minimal_payment ) VALUES('$accountNumber', '$creditLimit', '$minimumPayment');";
+        mysqli_query($connect, $query1);
+    }
 }
 
 //Course of action for loan
@@ -86,13 +133,59 @@ elseif($accountType =='loan'){
     $loanType = $_POST['loan-type'];
     $loanLimit = $_POST['loan-limit'];
 
-    //For different amounts of limit, we have different interest rates
+    //Initial interest rate
+    $interestRate= 2.0;
+
+    //For different amounts of limit, service type, loan type we have different interest rates
+    if($serviceType == 'banking'){
+        $interestRate += 2.0;
+    }
+    elseif ($serviceType =='investment'){
+        $interestRate += 1.0;
+    }
+    elseif ($serviceType == 'insurance'){
+        $interestRate += 0.5;
+    }
+
+    if($loanLimit == '5000.00'){
+        $interestRate += 2.0;
+    }
+    elseif($loanLimit == '10000.00'){
+        $interestRate += 1.5;
+    }
+    elseif($loanLimit == '15000.00'){
+        $interestRate += 1.0;
+    }
+    elseif($loanLimit == '25000.00'){
+        $interestRate += 0.5;
+    }
+
+    if($loanType == 'loan'){
+        $interestRate += 1.5;
+
+    }
+    elseif($loanType =='mortgage'){
+        $interestRate += 1.0;
+
+    }
+    elseif ($loanType == 'line-of-credit'){
+        $interestRate += 0.5;
+    }
 
     //Insert a new record into account
-
+    $query = "INSERT INTO Account(client_id, balance, account_type, service_type, level, interest_rate)
+                          VALUES('$client_id', '$balance', '$accountType', '$serviceType', '$level', '$interestRate');";
     //Insert a new record into Loan
+    if(mysqli_query($connect,$query)) {
+        $accountNumber = mysqli_insert_id($connect);
+
+        $query1 = "INSERT INTO Loan(account_number, loan_limit, type ) VALUES('$accountNumber', '$loanLimit', '$loanType');";
+        mysqli_query($connect, $query1);
+    }
 
 }
 
 mysqli_close($connect);
+
+echo "done.";
 
--- views/create_account.php
+++ views/create_account.php
@@ -9,37 +9,37 @@
 ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
-        $(function() {
-            $('#foreign-currency').hide();
-            $('#credit-card').hide();
-            $('#loan').hide();
-            $('#account-type').change(function(){
-                if ($('#account-type').val() == 'checking' || $('#account-type').val() == 'savings') {
-                    $('#foreign-currency').hide();
-                    $('#credit-card').hide();
-                    $('#loan').hide();
-                    $('#charge-plan').show();
-                }
-                else if($('#account-type').val() == 'foreign-currency') {
-                    $('#foreign-currency').show();
-                    $('#credit-card').hide();
-                    $('#loan').hide();
-                    $('#charge-plan').hide();
-                }
-                else if($('#account-type').val() == 'credit') {
-                    $('#credit-card').show();
-                    $('#charge-plan').hide();
-                    $('#loan').hide();
-                    $('#foreign-currency').hide();
-                }
-                else if($('#account-type').val() == 'loan') {
-                    $('#credit-card').hide();
-                    $('#charge-plan').hide();
-                    $('#loan').show();
-                    $('#foreign-currency').hide();
-                }
-            });
+    $(function() {
+        $('#foreign-currency').hide();
+        $('#credit-card').hide();
+        $('#loan').hide();
+        $('#account-type').change(function(){
+            if ($('#account-type').val() == 'checking' || $('#account-type').val() == 'savings') {
+                $('#foreign-currency').hide();
+                $('#credit-card').hide();
+                $('#loan').hide();
+                $('#charge-plan').show();
+            }
+            else if($('#account-type').val() == 'foreign-currency') {
+                $('#foreign-currency').show();
+                $('#credit-card').hide();
+                $('#loan').hide();
+                $('#charge-plan').hide();
+            }
+            else if($('#account-type').val() == 'credit') {
+                $('#credit-card').show();
+                $('#charge-plan').hide();
+                $('#loan').hide();
+                $('#foreign-currency').hide();
+            }
+            else if($('#account-type').val() == 'loan') {
+                $('#credit-card').hide();
+                $('#charge-plan').hide();
+                $('#loan').show();
+                $('#foreign-currency').hide();
+            }
         });
+    });
 </script>
 
 <h1>Create an Account</h1>
@@ -59,8 +59,9 @@
         <option value="insurance">Insurance</option>
     </select>
 
+    <!--TODO Credit should only appear if joining_date in $_SESSION is bigger than 6 months -->
     <p>Select an account type</p>
-    <select name="account-type", id="account-type">
+    <select name="account-type" id="account-type">
         <option value="checking">Checking</option>
         <option value="savings">Savings</option>
         <option value="foreign-currency">Foreign Currency</option>
@@ -70,28 +71,30 @@
 
     <div id="charge-plan">
         <p>Select a charge plan for your account</p>
-        <input type="radio", name="charge-plan", value="basic" checked>Basic<br>
-        <input type="radio", name="charge-plan", value="student">Student<br>
-        <input type="radio", name="charge-plan", value="premium">Premium<br>
-        <input type="radio", name="charge-plan", value="platinum">Platinum<br>
-        <input type="radio", name="charge-plan", value="senior">Senior<br>
+        <input type="radio" name="charge-plan" value="basic" checked>Basic<br>
+        <input type="radio" name="charge-plan" value="student">Student<br>
+        <input type="radio" name="charge-plan" value="premium">Premium<br>
+        <input type="radio" name="charge-plan" value="platinum">Platinum<br>
+        <input type="radio" name="charge-plan" value="senior">Senior<br>
     </div>
 
     <div id="foreign-currency">
         <p>Select a foreign currency</p>
-        <input type="radio", name="currency", value="usd">USD<br>
-        <input type="radio", name="currency", value="jpy">JPY<br>
-        <input type="radio", name="currency", value="eur">EUR<br>
-        <input type="radio", name="currency", value="cny">CNY<br>
+        <input type="radio" name="currency" value="usd">USD<br>
+        <input type="radio" name="currency" value="jpy">JPY<br>
+        <input type="radio" name="currency" value="eur">EUR<br>
+        <input type="radio" name="currency" value="cny">CNY<br>
     </div>
 
     <div id="credit-card">
         <p>Select a credit card limit</p>
-        <input type="radio", name="credit-limit", value="500" checked>500$<br>
-        <input type="radio", name="credit-limit", value="1000">1000$<br>
-        <input type="radio", name="credit-limit", value="50000">5000$<br>
-        <input type="radio", name="credit-limit", value="10000">10 000$<br>
+        <input type="radio" name="credit-limit" value="500.00" checked>500$<br>
+        <input type="radio" name="credit-limit" value="1000.00">1000$<br>
+        <input type="radio" name="credit-limit" value="5000.00">5000$<br>
+        <input type="radio" name="credit-limit" value="10000.00">10 000$<br>
     </div>
+
+    <!--TODO Line-of-Credit should only appear if joining_date in $_SESSION is bigger than 12 months -->
 
     <div id="loan">
         <p>Select a loan type</p>
@@ -102,11 +105,10 @@
         </select>
 
         <p>Select a limit type</p>
-        <input type="radio", name="loan-limit", value="5000">5000$<br>
-        <input type="radio", name="loan-limit", value="10000">10 000$<br>
-        <input type="radio", name="loan-limit", value="15000">15 000$<br>
-        <input type="radio", name="loan-limit", value="25000">25 000$<br>
+        <input type="radio" name="loan-limit" value="5000.00">5000$<br>
+        <input type="radio" name="loan-limit" value="10000.00">10 000$<br>
+        <input type="radio" name="loan-limit" value="15000.00">15 000$<br>
+        <input type="radio" name="loan-limit" value="25000.00">25 000$<br>
     </div><br>
     <input type="submit">
 </form>
-
