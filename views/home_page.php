dd1f2a77f4aa63eaddb9f3971c15022e03604efb
Viktoriya Markutsa <v_oblivion@hotmail.com>
2018-11-15 16:17:24 -0500

    removed message

--- views/home_page.php
+++ views/home_page.php
@@ -1,5 +1,4 @@
 <!-- if you need user information, just put them into the $_SESSION variable and output them here -->
 <?php include("menu.php"); ?>
-Try to close this browser tab and open it again. Still logged in! ;)
 
 
