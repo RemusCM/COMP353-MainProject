a675c6a6d4fd56e0a83b8e8590575c49a175d8a5
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2018-11-19 18:57:04 -0500

    #9 Each type of account now has its own table. Need to retrieve client ID to display accounts of client currently logged in.

--- views/list_accounts.php
+++ views/list_accounts.php
@@ -12,7 +12,8 @@
          * Date: 2018-11-19
          * Time: 17:54
          */
-
+        session_start();
+        $clientId = $_SESSION['client_id'];
 
         // Create connection
         $conn = mysqli_connect("localhost", "root","root","testaccount");
@@ -21,18 +22,43 @@
             die("Connection failed: " . $conn->connect_error);
         }
 
-        $sql = "SELECT account_type, account_number, balance, service_type, level FROM account";
-        $result = $conn->query($sql);
+        //TODO retrieve data from Loan, Credit and Foreign Currency and display it
+        //TODO Make sure that what is displayed in the table is for the client that is logged in.
 
-        if ($result->num_rows > 0) {
-            echo "<table class='table'><tr><th scope=\"col\">Account Type</th><th scope=\"col\">Account Number</th><th scope=\"col\">Balance</th><th scope=\"col\">Service Type</th><th scope=\"col\">Level</th>";
+        //Retrieving client id
+        $sql_clientId = "SELECT client_id FROM client";
+        $result_clientId = $conn->query($sql_clientId);
+
+        //Checking accounts query
+        $sql_checking = "SELECT DISTINCT checking.account_number, opt, balance, service_type, level FROM account, checking";
+        $result_checking = $conn->query($sql_checking);
+
+        //Savings accounts query
+        $sql_savings = "SELECT DISTINCT savings.account_number, opt, balance, service_type, level FROM account, savings";
+        $result_savings = $conn->query($sql_savings);
+
+        //Displaying checking accounts
+        if ($result_checking->num_rows > 0 && $clientId = $result_clientId) {
+            echo "<table class='table'><caption>Checking Accounts</caption><tr><th scope=\"col\">Account Number</th><th scope=\"col\">Balance</th><th scope=\"col\">Option</th><th scope=\"col\">Service Type</th><th scope=\"col\">Level</th>";
             // output data of each row
-            while($row = $result->fetch_assoc()) {
-                echo "<tr><td scope=\"row\">".$row["account_type"]."</td><td>".$row["account_number"]."</td><td>".$row["balance"]."</td><td>".$row["service_type"]."</td><td>".$row["level"]."</td></tr>";
+            while($row = $result_checking->fetch_assoc()) {
+                echo "<tr><td>".$row["account_number"]."</td><td>".$row["balance"]."</td><td>".$row["opt"]."</td><td>".$row["service_type"]."</td><td>".$row["level"]."</td></tr>";
             }
             echo "</table>";
-        } else {
-            echo "0 results";
+        }
+
+        //Displaying savings accounts
+        if ($result_savings->num_rows > 0 && $clientId = $result_clientId) {
+            echo "<table class='table'><caption>Savings Accounts</caption><tr><th scope=\"col\">Account Number</th><th scope=\"col\">Balance</th><th scope=\"col\">Option</th><th scope=\"col\">Service Type</th><th scope=\"col\">Level</th>";
+            // output data of each row
+            while($row = $result_savings->fetch_assoc()) {
+                echo "<tr><td>".$row["account_number"]."</td><td>".$row["balance"]."</td><td>".$row["opt"]."</td><td>".$row["service_type"]."</td><td>".$row["level"]."</td></tr>";
+            }
+            echo "</table>";
+        }
+
+        else {
+            echo "";
         }
         $conn->close();
         ?>
