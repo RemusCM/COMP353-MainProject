63f8c3daa2e35a44104a5870a09b86bf3cbd7e35
Remus Mocanu <Remus_C.M@hotmail.com>
2018-11-28 23:45:34 -0500

    #46 merged both triggers into one so it works on school server.

--- monthly_reccurring_charges.sql
+++ monthly_reccurring_charges.sql
@@ -20,28 +20,12 @@ UPDATE account a
 SET a.balance = balance - cp.charge WHERE a.account_type = 'savings';
 
 
-CREATE TRIGGER extraTransactionSavings
+
+DELIMITER //
+CREATE TRIGGER extraTransaction
 AFTER INSERT ON transaction
 FOR EACH ROW
-UPDATE account a	
-JOIN savings s ON a.account_number = s.account_number
-JOIN chargeplan cp on s.opt = cp.opt
-SET a.balance = a.balance-cp.additional_charge
-WHERE a.account_type = 'savings' AND a.account_number = NEW.account_number AND NEW.account_number IN (SELECT account_number
-FROM( SELECT t.account_number, COUNT(*), cp.lim
-FROM account a
-INNER JOIN savings s ON a.account_number = s.account_number
-INNER JOIN chargeplan cp ON s.opt=cp.opt
-INNER JOIN transaction t ON a.account_number = t.account_number
-where t.date > date_sub(now(), interval 1 month)
-GROUP BY t.account_number
-HAVING COUNT(*)>cp.lim) AS T);
-
-
-
-CREATE TRIGGER extraTransactionChecking
-AFTER INSERT ON transaction
-FOR EACH ROW
+BEGIN
 UPDATE account a	
 JOIN checking c ON a.account_number = c.account_number
 JOIN chargeplan cp on c.opt = cp.opt
@@ -55,6 +39,21 @@ INNER JOIN transaction t ON a.account_number = t.account_number
 where t.date > date_sub(now(), interval 1 month)
 GROUP BY t.account_number
 HAVING COUNT(*)>cp.lim) AS T);
+UPDATE account a	
+JOIN savings s ON a.account_number = s.account_number
+JOIN chargeplan cp on s.opt = cp.opt
+SET a.balance = a.balance-cp.additional_charge
+WHERE a.account_type = 'savings' AND a.account_number = NEW.account_number AND NEW.account_number IN (SELECT account_number
+FROM( SELECT t.account_number, COUNT(*), cp.lim
+FROM account a
+INNER JOIN savings s ON a.account_number = s.account_number
+INNER JOIN chargeplan cp ON s.opt=cp.opt
+INNER JOIN transaction t ON a.account_number = t.account_number
+where t.date > date_sub(now(), interval 1 month)
+GROUP BY t.account_number
+HAVING COUNT(*)>cp.lim) AS T);
+END//
+DELIMITER;
 
 
 
