13508764fa6eb2cc2fc77313e478d0bca259575b
Remus Mocanu <Remus_C.M@hotmail.com>
2018-11-28 19:36:39 -0500

    #46 added trigger in case user passes limit or charge plan transactions.

--- monthly_reccurring_charges.sql
+++ monthly_reccurring_charges.sql
@@ -20,7 +20,49 @@ UPDATE account a
 SET a.balance = balance - cp.charge WHERE a.account_type = 'savings';
 
 
+CREATE TRIGGER extraTransactionSavings
+AFTER INSERT ON transaction
+FOR EACH ROW
+UPDATE account a	
+JOIN savings s ON a.account_number = s.account_number
+JOIN chargeplan cp on s.opt = cp.opt
+SET a.balance = a.balance-cp.additional_charge
+WHERE a.account_type = 'savings' AND a.account_number = NEW.account_number AND NEW.account_number IN (SELECT account_number
+FROM( SELECT t.account_number, COUNT(*), cp.lim
+FROM account a
+INNER JOIN savings s ON a.account_number = s.account_number
+INNER JOIN chargeplan cp ON s.opt=cp.opt
+INNER JOIN transaction t ON a.account_number = t.account_number
+where t.date > date_sub(now(), interval 1 month)
+GROUP BY t.account_number
+HAVING COUNT(*)>cp.lim) AS T);
+
+
+
+CREATE TRIGGER extraTransactionChecking
+AFTER INSERT ON transaction
+FOR EACH ROW
+UPDATE account a	
+JOIN checking c ON a.account_number = c.account_number
+JOIN chargeplan cp on c.opt = cp.opt
+SET a.balance = a.balance-cp.additional_charge
+WHERE a.account_type = 'checking' AND a.account_number = NEW.account_number AND NEW.account_number IN (SELECT account_number
+FROM( SELECT t.account_number, COUNT(*), cp.lim
+FROM account a
+INNER JOIN checking c ON a.account_number = c.account_number
+INNER JOIN chargeplan cp ON c.opt=cp.opt
+INNER JOIN transaction t ON a.account_number = t.account_number
+where t.date > date_sub(now(), interval 1 month)
+GROUP BY t.account_number
+HAVING COUNT(*)>cp.lim) AS T);
+
+
+
 SHOW EVENTS;
 
+
+DROP TRIGGER IF EXISTS extraTransactionChecking;
+DROP TRIGGER IF EXISTS extraTransactionSavings;
 DROP EVENT IF EXISTS monthly_charge_checking;
 DROP EVENT IF EXISTS monthly_charge_savings;
+
